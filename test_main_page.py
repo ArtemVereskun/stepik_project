3a797a51c9b009fd7ae5d74e7cce157cbc1c4c22
artem vereskun <artem.vereskun@hotline.finance>
2024-03-04 22:41:26 +0200

    add try/except in is_element_present

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,5 @@
-
-from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -11,3 +10,10 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     # выполняем метод страницы — переходим на страницу логина
     page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
