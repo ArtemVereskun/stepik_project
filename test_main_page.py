c89aee20f48459c53e34678f48b17303dd78d085
artem vereskun <artem.vereskun@hotline.finance>
2024-04-29 07:19:56 +0300

    add LoginPage and LoginPage tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 
@@ -17,3 +18,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/uk/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
